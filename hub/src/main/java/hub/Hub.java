3fe11e68613213895b8f996ad11be51ace03d995
Rowanvb <Rowanvanbeckhoven@gmail.com>
2018-04-01 16:40:07 +0200

    neighbour & emergency service settings, init ip validation

--- hub/src/main/java/hub/Hub.java
+++ hub/src/main/java/hub/Hub.java
@@ -1,24 +1,36 @@
 package hub;
 
+import hub.settings.Neighbour;
+import hub.settings.Settings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpStatusCodeException;
+import org.springframework.web.client.RestTemplate;
 import status.Status;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Objects;
 
 @Component
 public class Hub extends JFrame {
 
     private Status status = Status.OK;
     private final JLabel label;
+    private Settings settings;
+    private String address;
 
     @Autowired
-    public Hub(@Value("${host:localhost}") final String host) {
+    public Hub(@Value("${host:localhost}") final String host, final Settings settings) {
         super("Hub @" + host);
-
-        setPreferredSize(new Dimension(600, 200));
+        this.settings = settings;
+        this.address = host;
+        setPreferredSize(new Dimension(800, 400));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JFrame.setDefaultLookAndFeelDecorated(true);
         setLocationRelativeTo(null);
@@ -28,8 +40,60 @@ public class Hub extends JFrame {
         label.setFont(new Font("Serif", Font.PLAIN, 50));
         label.setPreferredSize(new Dimension(200, 200));
         label.setHorizontalAlignment(SwingConstants.CENTER);
-        getContentPane().add(label, BorderLayout.CENTER);
+        add(label, BorderLayout.NORTH);
 
+        final JPanel addresses = new JPanel(new GridLayout(1, 2));
+
+        final JList<Neighbour> neighbourJList = new JList<>(settings.getNeighbours());
+        JScrollPane pane = new JScrollPane(neighbourJList);
+        neighbourJList.addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_DELETE) {
+                    int idx = neighbourJList.getSelectedIndex();
+                    if (idx != -1) {
+                        settings.getNeighbours().remove(idx);
+                    }
+                }
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+            }
+        });
+        addresses.add(pane);
+
+        final JLabel emergencyServices = new JLabel(settings.getEmergencyService());
+        addresses.add(emergencyServices);
+
+        add(addresses, BorderLayout.SOUTH);
+
+
+        final JComboBox<String> options = new JComboBox<>(new String[]{"Neighbour", "Emergency Service"});
+
+        final JTextField input = new JFormattedTextField();
+        input.setPreferredSize(new Dimension(600, 50));
+        input.addActionListener((e) -> {
+            final String type = options.getSelectedIndex() == 1 ? "emergency_service" : "neighbour";
+            if (addAddressToSettings(input.getText(), type)) {
+                if (Objects.equals(type, "emergency_service")) {
+                    emergencyServices.setText(input.getText());
+                }
+                input.setText("");
+            }
+        });
+
+        final JPanel addressPanel = new JPanel(new GridLayout(1, 3));
+        addressPanel.add(input);
+        addressPanel.add(options);
+
+        add(addressPanel, BorderLayout.CENTER);
+
+        setResizable(false);
         pack();
         setVisible(true);
     }
@@ -52,5 +116,21 @@ public class Hub extends JFrame {
 
     public Status getStatus() {
         return status;
+    }
+
+    private boolean addAddressToSettings(final String message, final String type) {
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> request = new HttpEntity<>(message);
+        try {
+            restTemplate.exchange("http://" + address + ":8080/api/settings/" + type,
+                    HttpMethod.POST, request, String.class);
+        } catch (HttpStatusCodeException e) {
+            System.err.println("Address is invalid.. ");
+            return false;
+        } catch (Exception e) {
+            System.err.println("Internal Server Error.. ");
+            return false;
+        }
+        return true;
     }
 }
--- hub/src/main/java/hub/RestHandler.java
+++ hub/src/main/java/hub/RestHandler.java
@@ -1,10 +1,17 @@
 package hub;
 
+import hub.settings.Neighbour;
+import hub.settings.Settings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import status.Status;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.regex.Pattern;
 
 @RestController
 @RequestMapping("/api")
@@ -12,9 +19,12 @@ public class RestHandler {
 
     private final Hub hub;
 
+    private final Settings settings;
+
     @Autowired
-    public RestHandler(Hub hub) {
+    public RestHandler(Hub hub, Settings settings) {
         this.hub = hub;
+        this.settings = settings;
     }
 
     // Endpoint for opening the lock
@@ -34,14 +44,15 @@ public class RestHandler {
     }
 
     // Endpoint for marking the emergency as resolved
-    @RequestMapping(value = "/emergency/neighbourComing", method = RequestMethod.POST)
+    @RequestMapping(value = "/emergency/neighbourComing", method = RequestMethod.GET)
     @ResponseBody
-    public ResponseEntity<String> updateEmergency(@RequestBody String timestamp) {
+    public ResponseEntity<String> updateEmergency() {
         // Verify user who sent request
-        System.out.println(timestamp);
-        // Stop emergency (close em. socket etc.)
-        System.out.println("Emergency PUT method called");
-        return new ResponseEntity<>(timestamp + " Updated ", HttpStatus.OK);
+
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+                .getRequest();
+        System.out.println("Sent from: " + request.getRemoteAddr());
+        return new ResponseEntity<>(" Updated ", HttpStatus.OK);
     }
 
     // Endpoint for changing the settings
@@ -68,4 +79,35 @@ public class RestHandler {
         return new ResponseEntity<>(test, HttpStatus.OK);
     }
 
+    @PostMapping(value = "/settings/neighbour")
+    @ResponseBody
+    public ResponseEntity<?> createNeighbour(@RequestBody String address) {
+        if (validate(address)) {
+            final Neighbour neighbour = new Neighbour();
+            neighbour.setAddress(address);
+            neighbour.setComing(false);
+            settings.addNeighbour(neighbour);
+            return new ResponseEntity<>(address, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Invalid address", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @PostMapping(value = "/settings/emergency_service")
+    @ResponseBody
+    public ResponseEntity<?> createEmergencyService(@RequestBody String address) {
+        if (validate(address)) {
+            settings.setEmergencyService(address);
+            return new ResponseEntity<>(address, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("Invalid address", HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    //TODO extract to Util
+
+    private static final Pattern PATTERN = Pattern.compile(
+            "^(([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.){3}([01]?\\d\\d?|2[0-4]\\d|25[0-5])$");
+
+    public static boolean validate(final String ip) {
+        return PATTERN.matcher(ip).matches();
+    }
 }
